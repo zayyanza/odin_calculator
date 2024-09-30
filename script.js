a0c269b14fd938c3ff6a93328f037996b0d9018c
zetampo <zbanjar9@gmail.com>
2024-09-30 12:48:17 +0700

    create operational arithmetic functions

---
+++ script.js
@@ -0,0 +1,22 @@
+let firstNumber;
+let secondNumber;
+let operator;
+
+function add(a, b) {
+    return a + b;
+}
+
+function substract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+let x = divide(2, 3);
+console.log(x);
