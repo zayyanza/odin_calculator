3fe1629a5c8fd52057905b2e2228bf88fa7d0c92
zetampo <zbanjar9@gmail.com>
2024-09-30 12:55:38 +0700

    create operate function

--- script.js
+++ script.js
@@ -18,5 +18,25 @@ function divide(a, b) {
     return a / b;
 }
 
-let x = divide(2, 3);
-console.log(x);
+
+function operate(firstNumber, secondNumber, operator) {
+    switch (operator) {
+        case "+":
+            return add(firstNumber, secondNumber);
+            break;
+        case "-":
+            return substract(firstNumber, secondNumber);
+            break;
+        case "*":
+            return multiply(firstNumber, secondNumber);
+            break;
+        case "/":
+            return divide(firstNumber, secondNumber);
+            break;
+        default:
+            break;
+    }
+}
+
+// let x = operate(5, 2, "/");
+// console.log(x);
